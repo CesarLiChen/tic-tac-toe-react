dbc78540043f394b5cf932e6482e08a13bb666f0
Cesar Li Chen <cesarli619@gmail.com>
2023-06-17 01:15:33 -0400

    Creates Square component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Square from "./components/Square";
 
 function App() {
   return (
     <div className="App">
-      Tic-Tac-Toe
+      <Square />
     </div>
   );
 }
