da03fab992a9f825b118c17b1d6238f631de2f57
Cesar Li Chen <cesarli619@gmail.com>
2023-06-19 21:04:43 -0400

    Adds code to determine winner

--- src/components/Game.js
+++ src/components/Game.js
@@ -25,6 +25,26 @@ function Game() {
     setCurrentPlayer(currPlayer === X_SYMBOL ? O_SYMBOL : X_SYMBOL);
   }
 
+  function getWinner() {
+    const winningCombinations = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
+    ];
+
+    const winningCombo = winningCombinations.find(combo => {
+      if(board[combo[0]] && 
+        board[combo[0]] === board[combo[1]] &&
+        board[combo[0]] === board[combo[2]]) {
+          return true;
+      } else {
+        return false;
+      }
+    });
+    return winningCombo ? board[winningCombo[0]] : false;
+  }
+
+  console.log(getWinner());
   return (
     <div>
       <Board 
@@ -32,6 +52,7 @@ function Game() {
         onBoardClick={handleBoardClick}
       />
       {currPlayer}
+      {getWinner()}
     </div>
     
   );
